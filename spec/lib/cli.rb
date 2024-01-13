50a83676789277deb43f85d129b17a97383a5e81
Deborah-McVey <139592532+Deborah-McVey@users.noreply.github.com>
2024-01-13 23:49:52 +0000

    Initial commit

---
+++ spec/lib/cli.rb
@@ -0,0 +1,28 @@
+require_relative "./scraper.rb"
+
+class CLI
+  def start
+    Scraper.scrape_countries #<--added this line
+    puts "Welcome to the Countries of the World CLI!"
+    puts "What is your name?"
+    name = gets.strip
+    puts "Hello #{name}!"
+#add these lines
+    puts "Please enter a country name to get more information about it."
+    input = gets.strip
+    country = Country.all.find { |country| country.name.downcase == input.downcase }
+    if country === nil
+      puts "Sorry, that country is not in our database. Please try again."
+    else
+      puts "Name: #{country.name}"
+      puts "Capital: #{country.capital}"
+      puts "Population: #{country.population}"
+      puts "Area: #{country.area}"
+    end
+   #added lines above 
+  end
+
+  def get_input
+    gets.strip
+  end
+end
